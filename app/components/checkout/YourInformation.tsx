f32619599b43390d96093db07e6a52736b4a3668
Mijanur Rahman <mijan.connect@gmail.com>
2026-01-12 11:49:20 +0600

    Address details section designed

--- app/components/checkout/YourInformation.tsx
+++ app/components/checkout/YourInformation.tsx
@@ -1,3 +1,4 @@
+import AddressDetails from "./second-step/AddressDetails";
 import MainDriverDetails from "./second-step/MainDriverDetails";
 
 export default function YourInformation() {
@@ -5,6 +6,7 @@ export default function YourInformation() {
     <>
       <div className="mt-2 lg:mt-6">
         <MainDriverDetails />
+        <AddressDetails />
       </div>
     </>
   );
