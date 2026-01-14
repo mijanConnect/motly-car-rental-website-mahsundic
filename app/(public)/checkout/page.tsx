0615fea98ab2ac94178d54c9de8b2cadbfcf6e6e
Mijanur Rahman <mijan.connect@gmail.com>
2026-01-14 17:02:12 +0600

    Build error fixed

--- app/(public)/checkout/page.tsx
+++ app/(public)/checkout/page.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import Checkout from "@/app/components/checkout/Checkout";
+
+function CheckoutFallback() {
+  return <div className="my-5 lg:my-12">Loading...</div>;
+}
 
 export default function Page() {
   return (
     <>
-      <div className="my-5 lg:my-12">
-        <Checkout />
-      </div>
+      <Suspense fallback={<CheckoutFallback />}>
+        <div className="my-5 lg:my-12">
+          <Checkout />
+        </div>
+      </Suspense>
     </>
   );
 }
