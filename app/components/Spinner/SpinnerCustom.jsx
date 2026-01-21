05fd1c0955df2a2f07cc7cb912276c61024e30bd
Mijanur Rahman <mijan.connect@gmail.com>
2026-01-21 15:20:30 +0600

    Spinner fixed

--- app/components/Spinner/SpinnerCustom.jsx
+++ app/components/Spinner/SpinnerCustom.jsx
@@ -2,7 +2,7 @@ import "./spinner.css";
 
 export default function SpinnerCustom() {
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-white z-50">
+    <div className="flex justify-center items-center bg-white">
       <span className="loader"></span>
     </div>
   );
